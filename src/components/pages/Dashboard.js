80179f032caff1f611397e34a8594e94c7f82e49
Karlo Zrilić <kazrile@gmail.com>
2020-10-06 20:59:10 +0200

    UPDATES

--- src/components/pages/Dashboard.js
+++ src/components/pages/Dashboard.js
@@ -3,12 +3,17 @@ import axios from 'axios';
 import { Form, Button, Alert } from 'react-bootstrap';
 import jwt_decode from "jwt-decode";
 import ReactLoading from 'react-loading';
+import moment from 'moment';
 
 const TITLE = "Dashboard";
 
 function Dashboard(props) {
 
     const [data, setData] = useState([])
+    const [dates, setDates] = useState({
+        created_at: "",
+        updated_at: ""
+    })
     const [state, setState] = useState({
         isLoading: true
     })
@@ -26,11 +31,14 @@ function Dashboard(props) {
             const res = await axios.post(
                 'https://zrilich.pythonanywhere.com/api/v1/get-user-details', data, config
             );
-            setData(res.data)
+            setData(jwt_decode(res.data))
+            setDates({
+                created_at: jwt_decode(res.data).created_at,
+                updated_at: jwt_decode(res.data).updated_at
+            })
             setState({
                 isLoading: false
             })
-        
         }
         loginSubmit();
     }, []);
@@ -50,6 +58,11 @@ function Dashboard(props) {
             <>
                 <div className="loginForm">
                     <h1 className="pb-3">Dashboard</h1>
+                    <p>First Name: {data.first_name}</p>
+                    <p>Last Name: {data.last_name}</p>
+                    <p>Email: {data.email}</p>
+                    <small>Created at: {moment(dates.created_at).format('DD/MM/YYYY, HH:mm:ss')}</small>
+                    <small>Updated at: {moment(dates.updated_at).format('DD/MM/YYYY, HH:mm:ss')}</small>
                 </div>
             </>
         )
