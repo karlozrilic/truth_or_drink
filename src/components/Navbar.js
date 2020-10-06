3330904fbd179fa4704b545094f6de9ede9ef436
Karlo Zrilić <kazrile@gmail.com>
2020-10-06 22:31:00 +0200

    updates

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -5,20 +5,20 @@ function Navbar(props) {
 
     return (
         <>
-        <NavbarBoot className="fixed-top text-center" variant="dark" bg="dark" expand="lg">
+        <NavbarBoot className="fixed-top text-center" variant="light" bg="warning" expand="lg">
             <NavbarBoot.Brand href="/" className="navbar-logo">
-                <img src={process.env.PUBLIC_URL + '/logo-light.webp'} /*src="https://getbootstrap.com/docs/4.5/assets/brand/bootstrap-solid.svg"*/ width="150" height="45" className="d-inline-block align-top" alt="" loading="lazy" />
+                <img src={process.env.PUBLIC_URL + '/logo-dark.webp'} /*src="https://getbootstrap.com/docs/4.5/assets/brand/bootstrap-solid.svg"*/ width="150" height="45" className="d-inline-block align-top" alt="" loading="lazy" />
             </NavbarBoot.Brand>
             <NavbarBoot.Toggle aria-controls="basic-navbar-nav" />
             <NavbarBoot.Collapse id="basic-navbar-nav" className="justify-content-end">
                 <Nav className="mr-auto" className="justify-content-end">
-                    <Nav.Link href="/">Home</Nav.Link>
+                    <Nav.Link href="/" >Home</Nav.Link>
                     {!props.valid ?
-                        <Nav.Link href="/login">Login</Nav.Link>
+                        <Nav.Link href="/login" >Login</Nav.Link>
                     :
                     <>
-                        <Nav.Link href="/dashboard">Dashboard</Nav.Link>
-                        <Nav.Link href="/logout">Log Out</Nav.Link>
+                        <Nav.Link href="/dashboard" >Dashboard</Nav.Link>
+                        <Nav.Link href="/logout" >Log Out</Nav.Link>
                     </>
                     }
                     <Nav.Link href="/help">Help</Nav.Link>
