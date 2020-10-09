d4250b2cf4972e3a6a88f78b4ba1aace1e827d24
Karlo Zrilić <kazrile@gmail.com>
2020-10-09 20:42:25 +0200

    update

--- src/App.js
+++ src/App.js
@@ -28,6 +28,9 @@ function App() {
   const bckp = 'https://hn.algolia.com/api/v1/search?query=redux';
   const url = 'https://zrilich.pythonanywhere.com/api/v1/extraDirty/all';
 
+  let vh = window.innerHeight * 0.01;
+  document.documentElement.style.setProperty('--vh', `${vh}px`);
+
   const footerUrls = ["/", "/dashboard/user-info", "/dashboard/suggest-question", "/dashboard/my-suggestions", "/how-to-play", "/categories"]
 
   if (localStorage.token == null) {
