d5f8b1bd61caed72aa9bcd3ebd2dae5fdade166e
karlozrilic <kazrile@gmail.com>
2021-02-12 14:22:32 +0100

    Updates

---
+++ precache-manifest.ef2d4cca6ded47474900aea1e965a8e6.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a2c5edd83a42b126ed5c072d0b76f6ee",
+    "url": "/truth_or_drink/index.html"
+  },
+  {
+    "revision": "7e0b39cba1cd36cd32e0",
+    "url": "/truth_or_drink/static/css/main.dbedc17c.chunk.css"
+  },
+  {
+    "revision": "fcde6f621d94d8756f3c",
+    "url": "/truth_or_drink/static/js/2.89c23305.chunk.js"
+  },
+  {
+    "revision": "097cbef6484f04fd31ecf76946fd94ac",
+    "url": "/truth_or_drink/static/js/2.89c23305.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7e0b39cba1cd36cd32e0",
+    "url": "/truth_or_drink/static/js/main.e0336cab.chunk.js"
+  },
+  {
+    "revision": "a74800574cf00da9bfb7",
+    "url": "/truth_or_drink/static/js/runtime-main.03885da8.js"
+  },
+  {
+    "revision": "63b3eadf39f42942b2012af7b25351b4",
+    "url": "/truth_or_drink/static/media/404-error.63b3eadf.png"
+  },
+  {
+    "revision": "197a31e1acaca0901433c2a19a9594f4",
+    "url": "/truth_or_drink/static/media/home-bckg.197a31e1.jpg"
+  }
+]);
